dc1d12363bf8083ea7f6ac915df67f2e7737d03b
sharpennd <81886660+sharpennd@users.noreply.github.com>
2026-09-17 22:50:32 +0100

    .

--- org/sharpennd/minecraft-neofetch/MinecraftNeofetch.java
+++
@@ -1,9 +0,0 @@
-package org.kwesou.mcNeofetch;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public final class McNeofetch extends JavaPlugin {
-   public void onEnable() {
-      this.getCommand("neofetch").setExecutor(new sendOutput());
-   }
-}
